f6355467748ad0aed95c242b8eca54e80d8175a1
Bronz Developer <bronz.developer@gmail.com>
2019-01-13 21:13:50 +0530

    Initial commit

---
+++ Assets/OpenPose/Examples/Scripts/OpenPoseUserScript.cs
@@ -0,0 +1,195 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace OpenPose.Example
+{
+    public class OpenPoseUserScript : MonoBehaviour
+    {
+        // The 2D human to control
+        [SerializeField] Transform humanContainer;
+        [SerializeField] HumanController2D humanController2D;
+        [SerializeField] ImageRenderer imageRenderer;
+        [SerializeField] Text fpsText;
+        [SerializeField] Text peopleText;
+        [SerializeField] private bool showPeopleCount;
+        [SerializeField] private bool showFPS;
+        [SerializeField] private bool renderBgImg = true;
+        [SerializeField] [Range(0f, 1f)] private float frameRateSmoothRatio = 0.8f;
+        // User settings
+        [SerializeField] private ProducerType inputType = ProducerType.Webcam;
+        [SerializeField] private string producerString = "-1";
+        [SerializeField] private int maxPeople = 1;
+        [SerializeField] private float renderThreshold = 0.05f;
+        [SerializeField] private bool handEnabled = false;
+        [SerializeField] private bool faceEnabled = false;
+        [SerializeField] private Vector2Int netResolution = new Vector2Int(-1, 368);
+        [SerializeField] private Vector2Int handResolution = new Vector2Int(368, 368);
+        [SerializeField] private Vector2Int faceResolution = new Vector2Int(368, 368);
+
+        private int numberPeople = 0;
+
+        private float avgFrameRate = 0f;
+        private float avgFrameTime = -1f;
+        private float lastFrameTime = -1f;
+        private HumanController2D humanCont;
+        // Output control
+        private OPDatum datum;
+
+        private void Start()
+        {
+            humanCont = Instantiate(humanController2D, humanContainer);
+            print("Start");
+            // Register callbacks
+            OPWrapper.OPRegisterCallbacks();
+            // Enable OpenPose run multi-thread (default true)
+            OPWrapper.OPEnableMultiThread(true);
+            // Enable OpenPose log to unity (default true)
+            OPWrapper.OPEnableDebug(true);
+            // Enable OpenPose output to unity (default true)
+            OPWrapper.OPEnableOutput(true);
+            // Enable receiving image (default false)
+            OPWrapper.OPEnableImageOutput(renderBgImg);
+            imageRenderer.FadeInOut(renderBgImg);
+            // Configure OpenPose with default value, or using specific configuration for each
+            /* OPWrapper.OPConfigureAllInDefault(); */
+            UserConfigureOpenPose();
+
+            // Start OpenPose
+            OPWrapper.OPRun();
+        }
+
+        public void ApplyChanges()
+        {
+            // Restart OpenPose
+            StartCoroutine(UserRebootOpenPoseCoroutine());
+        }
+
+        public void ToggleRenderBgImg()
+        {
+            renderBgImg = !renderBgImg;
+            OPWrapper.OPEnableImageOutput(renderBgImg);
+            imageRenderer.FadeInOut(renderBgImg);
+        }
+
+        private void UserConfigureOpenPose()
+        {
+            OPWrapper.OPConfigurePose(
+                /* enable */ true, /* netInputSize */ netResolution, /* outputSize */ null,
+                /* keypointScaleMode */ ScaleMode.InputResolution,
+                /* gpuNumber */ -1, /* gpuNumberStart */ 0, /* scalesNumber */ 1, /* scaleGap */ 0.3f,
+                /* renderMode */ RenderMode.Gpu, /* poseModel */ PoseModel.BODY_25,
+                /* blendOriginalFrame */ true, /* alphaKeypoint */ 0.6f, /* alphaHeatMap */ 0.7f,
+                /* defaultPartToRender */ 0, /* modelFolder */ null,
+                /* heatMapTypes */ HeatMapType.None, /* heatMapScaleMode */ ScaleMode.UnsignedChar,
+                /* addPartCandidates */ false, /* renderThreshold */ renderThreshold, /* numberPeopleMax */ maxPeople,
+                /* maximizePositives */ false, /* fpsMax fps_max */ -1.0,
+                /* protoTxtPath */ "", /* caffeModelPath */ "");
+
+            OPWrapper.OPConfigureHand(
+                /* enable */ handEnabled, /* netInputSize */ handResolution,
+                /* scalesNumber */ 1, /* scaleRange */ 0.4f, /* tracking */ false,
+                /* renderMode */ RenderMode.None,
+                /* alphaKeypoint */ 0.6f, /* alphaHeatMap */ 0.7f, /* renderThreshold */ 0.2f);
+
+            OPWrapper.OPConfigureFace(
+                /* enable */ faceEnabled, /* netInputSize */ faceResolution, /* renderMode */ RenderMode.None,
+                /* alphaKeypoint */ 0.6f, /* alphaHeatMap */ 0.7f, /* renderThreshold */ 0.4f);
+
+            OPWrapper.OPConfigureExtra(
+                /* reconstruct3d */ false, /* minViews3d */ -1, /* identification */ false, /* tracking */ -1,
+                /* ikThreads */ 0);
+
+            OPWrapper.OPConfigureInput(
+                /* producerType */ inputType, /* producerString */ producerString,
+                /* frameFirst */ 0, /* frameStep */ 1, /* frameLast */ ulong.MaxValue,
+                /* realTimeProcessing */ false, /* frameFlip */ false,
+                /* frameRotate */ 0, /* framesRepeat */ false,
+                /* cameraResolution */ null, /* cameraParameterPath */ null,
+                /* undistortImage */ false, /* numberViews */ -1);
+
+            OPWrapper.OPConfigureOutput(
+                /* verbose */ -1.0, /* writeKeypoint */ "", /* writeKeypointFormat */ DataFormat.Yml,
+                /* writeJson */ "", /* writeCocoJson */ "", /* writeCocoFootJson */ "", /* writeCocoJsonVariant */ 1,
+                /* writeImages */ "", /* writeImagesFormat */ "png", /* writeVideo */ "", /* writeVideoFps */ 30.0,
+                /* writeHeatMaps */ "", /* writeHeatMapsFormat */ "png", /* writeVideo3D */ "",
+                /* writeVideoAdam */ "", /* writeBvh */ "", /* udpHost */ "", /* udpPort */ "8051");
+
+            OPWrapper.OPConfigureGui(
+                /* displayMode */ DisplayMode.NoDisplay, /* guiVerbose */ false, /* fullScreen */ false);
+        }
+
+        private IEnumerator UserRebootOpenPoseCoroutine()
+        {
+            if (OPWrapper.state == OPState.None) yield break;
+            // Shutdown if running
+            if (OPWrapper.state == OPState.Running)
+            {
+                OPWrapper.OPShutdown();
+            }
+            // Wait until fully stopped
+            yield return new WaitUntil(() => { return OPWrapper.state == OPState.Ready; });
+            // Configure and start
+            UserConfigureOpenPose();
+            OPWrapper.OPRun();
+        }
+
+        private void Update()
+        {
+            // New data received
+            if (OPWrapper.OPGetOutput(out datum))
+            {
+
+                // Draw human in data
+                int i = 0;
+                humanCont.DrawHuman(ref datum, i++, renderThreshold);
+                //foreach (var human in humanContainer.GetComponentsInChildren<HumanController2D>())
+                //{
+                //    //human.DrawHuman(ref datum, i++, renderThreshold);
+                //    humanCont.DrawHuman(ref datum, i++, renderThreshold);
+                //}
+
+                // Draw image
+                imageRenderer.UpdateImage(datum.cvInputData);
+
+                // Number of people
+                if (showPeopleCount)
+                {
+                    NumberOfPeople();
+                }
+                if (showFPS)
+                {
+                    CalculateFPS();
+                }
+            }
+        }
+
+        private void NumberOfPeople()
+        {
+            if (datum.poseKeypoints == null || datum.poseKeypoints.Empty())
+            {
+                numberPeople = 0;
+            }
+            else
+            {
+                numberPeople = datum.poseKeypoints.GetSize(0);
+            }
+            peopleText.text = "People: " + numberPeople;
+        }
+
+        private void CalculateFPS()
+        {
+            if (lastFrameTime > 0f)
+            {
+                if (avgFrameTime < 0f) avgFrameTime = Time.time - lastFrameTime;
+                else
+                {
+                    avgFrameTime = Mathf.Lerp(Time.time - lastFrameTime, avgFrameTime, frameRateSmoothRatio);
+                    avgFrameRate = 1f / avgFrameTime;
+                }
+            }
+            lastFrameTime = Time.time;
+            fpsText.text = " FPS: " + avgFrameRate.ToString("F1");
+        }
+    }
+}
---
+++ Assets/Scripts/MyWebCamTexture.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyWebCamTexture : MonoBehaviour
+{
+    public RawImage rawimage;
+
+    void Start()
+    {
+        WebCamTexture webcamTexture = new WebCamTexture();
+        rawimage.texture = webcamTexture;
+        rawimage.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+
+}
